ea08adc3f3be093c1cc2191e72f8d657d1be126e
axell-brendow <breno.axel@gmail.com>
2021-10-14 01:39:38 -0300

    Create interfaces for persistence and services

--- application/product.go
+++ application/product.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
 )
 
 func init() {
@@ -20,6 +21,26 @@ type IProduct interface {
 	GetPrice() float64
 }
 
+type IProduceService interface {
+	Get(id string) (IProduct, error)
+	Create(name string, price float64) (IProduct, error)
+	Enable(product IProduct) (IProduct, error)
+	Disable(product IProduct) (IProduct, error)
+}
+
+type IProductReader interface {
+	Get(id string) (IProduct, error)
+}
+
+type IProductWriter interface {
+	Save(product IProduct) (IProduct, error)
+}
+
+type IProductPersistence interface {
+	IProductReader
+	IProductWriter
+}
+
 const (
 	DISABLED = "disabled"
 	ENABLED  = "enabled"
@@ -30,6 +51,14 @@ type Product struct {
 	Name   string  `valid:"required"`
 	Status string  `valid:"required"`
 	Price  float64 `valid:"float,optional"`
+}
+
+func NewProduct() *Product {
+	product := Product{
+		Id:     uuid.NewV4().String(),
+		Status: DISABLED,
+	}
+	return &product
 }
 
 func (p *Product) IsValid() (bool, error) {
