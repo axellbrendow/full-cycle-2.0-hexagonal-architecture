039085faf5e2fc6328393d0e1a272c862dde50dd
axell-brendow <breno.axel@gmail.com>
2021-10-14 08:29:12 -0300

    Implement and test the remaining ProductService methods

--- application/product_service.go
+++ application/product_service.go
@@ -11,3 +11,54 @@ func (s *ProductService) Get(id string) (IProduct, error) {
 	}
 	return product, nil
 }
+
+func (s *ProductService) Create(name string, price float64) (IProduct, error) {
+	product := NewProduct()
+	product.Name = name
+	product.Price = price
+	_, err := product.IsValid()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Persistence.Save(product)
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, err
+}
+
+func (s *ProductService) Enable(product IProduct) (IProduct, error) {
+	err := product.Enable()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Persistence.Save(product)
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, nil
+}
+
+func (s *ProductService) Disable(product IProduct) (IProduct, error) {
+	err := product.Disable()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Persistence.Save(product)
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, nil
+}
--- application/product_service_test.go
+++ application/product_service_test.go
@@ -25,3 +25,44 @@ func TestProductService_Get(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, product, result)
 }
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockIProduct(ctrl)
+	persistence := mock_application.NewMockIProductPersistence(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Create("Product 1", 10)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+
+func TestProductService_EnableDisable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockIProduct(ctrl)
+	product.EXPECT().Enable().Return(nil).AnyTimes()
+	product.EXPECT().Disable().Return(nil).AnyTimes()
+
+	persistence := mock_application.NewMockIProductPersistence(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Enable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+	result, err = service.Disable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
